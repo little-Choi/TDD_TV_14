faf79620fc7779b9aead9f13faf5321a2da7c6fe
little-Choi <dkssudsdkssu@gmail.com>
2026-05-15 17:19:56 +0900

    ApprovalTest 생성

---
+++ build/CMakeFiles/TVControllerApprovalTest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TVControllerApprovalTest.dir/src/TVController.cpp.obj"
+  "CMakeFiles/TVControllerApprovalTest.dir/src/TVController.cpp.obj.d"
+  "CMakeFiles/TVControllerApprovalTest.dir/test/TVControllerApprovalTest.cpp.obj"
+  "CMakeFiles/TVControllerApprovalTest.dir/test/TVControllerApprovalTest.cpp.obj.d"
+  "TVControllerApprovalTest.exe"
+  "TVControllerApprovalTest.exe.manifest"
+  "TVControllerApprovalTest.pdb"
+  "TVControllerApprovalTest[1]_tests.cmake"
+  "libTVControllerApprovalTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TVControllerApprovalTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
